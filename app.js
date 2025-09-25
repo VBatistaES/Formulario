13d2bde55ed564e9a0d38e2d461af71c4a44a3d1
VBatistaES <vbatistaes@gmail.com>
2025-09-25 16:34:45 -0300

    Atualização no código e nas funções.

--- app.js
+++ app.js
@@ -1,57 +1,50 @@
-function SalvarNome() {
-    let nome = document.getElementById('nome').value;
-    if (nome === '') {
-        window.alert('Digite um nome')
-    }
-    else if (/^[0-9]+$/.test(nome)) {
-        alert(`O nome ${nome} Não e um nome! Por favor digitar um nome.`)
-    }
-    else {
-        window.alert(`O nome salvo foi: ${nome}`);
-        window.alert('Seja bem vindo a tela de cadastro, Continue o preenchimento.')
-    }
-}
-
-function SalvarCPF() {
-    let cpf = document.getElementById('cpf').value;
-    let cpfSeparado = cpf.length;
-    console.log(cpf)
-    console.log(cpfSeparado)
-    if (cpfSeparado != 11) {
-        window.alert(`O CPF ${cpf} esta incorreto!`)
-        window.alert('O número valido para CPF só pode conter 11 Dígitos.')
-    }
-    else if (/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/.test(cpf)) {
-        window.alert('O CPF não pode conter letras')
-    }
-    else {
-        window.alert(`O CPF de número ${cpf} foi salvo com sucesso!`)
-    }
-}
-
-function SalvarTelefone() {
-    let telefone = document.getElementById('telefone').value;
-    window.alert(`O seu telefone de número: ${telefone} foi salvo!`);
-}
-
-function SalvarAniversario() {
-    let aniversario = document.getElementById('aniversario').value;
-    window.alert(`O seu aniversario da data: ${aniversario} foi salvo!`);
-}
-
-function SalvarEmail() {
+function SalvarTudo() {//salva todos os dados fornecidos e faz a conferencia.
     let nome = document.getElementById('nome').value;
     let cpf = document.getElementById('cpf').value;
+    let cpfSeparado = cpf.length;//separa o cpf dígito por dígito.
     let telefone = document.getElementById('telefone').value;
     let aniversario = document.getElementById('aniversario').value;
     let email = document.getElementById('email').value;
-
     let TextoFinal = document.getElementById('TextoFinal');
-
-    if (email) {
-        window.alert(`O seu email : ${email} foi salvo com sucesso!`);
-        TextoFinal.innerHTML = `Olá ${nome}! Seu CPF é: ${cpf}, seu telefone é: ${telefone}, sua data de aniversário é: ${aniversario} e seu email é: ${email}`;
-    } else {
+    if (nome === '') {
+        window.alert('Digite um nome');
+    }
+    else if (/^[0-9]+$/.test(nome)) { //verifica se no nome não há números.
+        window.alert(`O nome ${nome} Não e um nome! Por favor digitar um nome.`)
+    }
+    else if (cpfSeparado != 11) { //verifica se o cpf separado e diferente de 11 dígitos
+        window.alert(`O CPF ${cpf} esta incorreto!`)
+        window.alert('O número valido para CPF deve conter 11 Dígitos.')
+    }
+    else if (/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/.test(cpf)) {//verifca se no cpf não há nenhuma letra.
+        window.alert('O CPF não pode conter letras!')
+    }
+    else if (telefone === '') {
+        window.alert('O Campo "Telefone" esta vazio.')
+    }
+    else if (aniversario === '') {
+        window.alert('O campo "Aniversário" esta vazio.')
+    }
+    else if (email === '') {
         window.alert('O campo de e-mail está vazio!');
     }
+    else {
+        TextoFinal.innerHTML = `Olá ${nome}! Seu CPF é: ${cpf}, seu telefone é: ${telefone}, sua data de aniversário é: ${aniversario} e seu email é: ${email}`;
+    }
+}
+
+function Limpar() {//limpa os dados digitados.
+    let nome = document.getElementById('nome');
+    let cpf = document.getElementById('cpf');
+    let telefone = document.getElementById('telefone');
+    let aniversario = document.getElementById('aniversario');
+    let email = document.getElementById('email');
+    let TextoFinal = document.getElementById('TextoFinal');
+
+    nome.value = "";//atualiza os valores que estão preenchidos com vazio.
+    cpf.value = "";
+    telefone.value = "";
+    aniversario.value = "";
+    email.value = "";
+    TextoFinal.innerText = "";
 }
